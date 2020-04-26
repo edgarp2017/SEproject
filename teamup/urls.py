d9cc6fa41f1d89b49b5a71ed2c340aa1cba92260
bryanr1992 <bryan.rivas05@gmail.com>
2020-04-26 19:52:40 -0400

    added a login nav-bar item and set it up so it goes to the login page on click

--- teamup/urls.py
+++ teamup/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='teamup-home'),
     path('settings/', views.settings, name='teamup-settings'),
+    path('users/login', views.login, name='teamup-login'),
 ]
