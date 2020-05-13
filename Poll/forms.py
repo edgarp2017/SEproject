5d4f33c502eac316b4d2f5fb4eb294de1d0e4df0
bryanr1992 <bryan.rivas05@gmail.com>
2020-05-13 10:06:13 -0700

    Results of the posts get posted to the group details page after everyone voted

--- Poll/forms.py
+++ Poll/forms.py
@@ -1,18 +1,32 @@
 from django.forms import ModelForm
 from.models import Poll
+from Groups.models import Group
+from Post.models import Post
 
 class CreatePollForm(ModelForm):
     def __init__(self, *args, **kwargs):
         self.group = kwargs.pop('group',None)
         self.user = kwargs.pop('request',None)
         super(CreatePollForm, self).__init__(*args, **kwargs)
-        print(self.user)
 
     class Meta:
         model = Poll
         fields = ['question', 'ans_one', 'ans_two', 'ans_three']
 
 
-    def checkVoted(self):
-        data = self.cleaned_data
-        poll = Polls.objects.get(pk=group.pk)
+    def delete(self):
+        group = Group.objects.get(name=self.group)
+        members = group.members.all().count()
+
+        poll = Poll.objects.get(Group=self.group)
+
+        if poll.total() == members:
+            desc = "Results from poll: %s, has %d votes, %s, has %d and %s has %d votes."%(poll.ans_one, poll.ans_one_votes, poll.ans_two, poll.ans_two_votes, poll.ans_three, poll.ans_three_votes)
+            Post.objects.create(group=self.group, title=poll.question, desc=desc, user=self.user)
+
+        poll.delete()
+
+
+
+
+
